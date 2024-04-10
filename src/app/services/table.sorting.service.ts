f3ebb17f19975727724affbe02860744e005bb92
amarko1 <amarko1@racunarstvo.hr>
2024-04-10 10:13:29 +0200

    Popravljen sorting za igraca

--- src/app/services/table.sorting.service.ts
+++ src/app/services/table.sorting.service.ts
@@ -1,9 +1,25 @@
 import {Injectable} from "@angular/core";
 import {MatTableDataSource} from "@angular/material/table";
 import {TableItem} from "../components/configuration/table.component.configuration";
+import {PlayerService} from "./player.service";
 
 @Injectable({ providedIn: 'root' })
 export class TableSortingService {
+
+  playerNameMap: Map<string, string> = new Map();
+
+  constructor(private playerService: PlayerService) {
+    this.fetchPlayerNames();
+  }
+
+  private fetchPlayerNames(): void {
+    this.playerService.getPlayers().subscribe(players => {
+      players.forEach(player => {
+        this.playerNameMap.set(player.id, player.firstName);
+      });
+    });
+  }
+
   sortData(dataSource: MatTableDataSource<TableItem>, sortColumn: string, sortDirection: 'asc' | 'desc') {
     const data = [...dataSource.data];
     data.sort((a, b) => this.compare(a, b, sortColumn, sortDirection));
@@ -14,9 +30,14 @@ export class TableSortingService {
     let valueA = this.getPropertyValue(a, sortColumn);
     let valueB = this.getPropertyValue(b, sortColumn);
 
-    if (!isNaN(Number(valueA)) && !isNaN(Number(valueB))) {
-      valueA = Number(valueA);
-      valueB = Number(valueB);
+    if (sortColumn === 'playerId') {
+      valueA = this.playerNameMap.get(valueA) || valueA;
+      valueB = this.playerNameMap.get(valueB) || valueB;
+    } else {
+      if (!isNaN(Number(valueA)) && !isNaN(Number(valueB))) {
+        valueA = Number(valueA);
+        valueB = Number(valueB);
+      }
     }
 
     const isAsc = sortDirection === 'asc';
